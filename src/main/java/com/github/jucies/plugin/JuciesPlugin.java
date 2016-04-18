d4e9c67dcd4878685f0a688dc3d0e8156e71efcf
Sergei Egorov <bsideup@gmail.com>
2016-04-18 21:02:06 +0300

    add Jucies only if not present >_<

--- src/main/java/com/github/jucies/plugin/JuciesPlugin.java
+++ src/main/java/com/github/jucies/plugin/JuciesPlugin.java
@@ -1,15 +1,48 @@
 package com.github.jucies.plugin;
 
 import hudson.init.Initializer;
+import hudson.model.UpdateCenter;
 import jenkins.model.Jenkins;
 
 import java.io.IOException;
 
+import static hudson.init.InitMilestone.PLUGINS_STARTED;
+
 public class JuciesPlugin {
 
-    @Initializer
+    @Initializer(after = PLUGINS_STARTED)
     public static void init() throws IOException {
+        UpdateCenter updateCenter = Jenkins.getInstance().getUpdateCenter();
+
+        boolean flush = false;
+
         // TODO alpha & beta channels
-        Jenkins.getInstance().getUpdateCenter().getSites().add(new SignaturelessUpdateSite("jucies", "https://jucies.github.io/update-center.json"));
+        Channel[] channels = {
+                new Channel("jucies", "https://jucies.github.io/update-center.json")
+        };
+
+        for (Channel channel : channels) {
+            if (updateCenter.getById(channel.id) != null) {
+                continue;
+            }
+
+            flush = true;
+            updateCenter.getSites().add(new SignaturelessUpdateSite(channel.id, channel.url));
+        }
+
+        if (flush) {
+            updateCenter.save();
+        }
+    }
+
+    private static final class Channel {
+        final String id;
+
+        final String url;
+
+        private Channel(String id, String url) {
+            this.id = id;
+            this.url = url;
+        }
     }
 }
